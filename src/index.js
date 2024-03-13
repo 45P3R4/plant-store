aea99a072be53cc46c6c8288c95a4ca19a1f1879
45P3R4 <langleybaka@gmail.com>
2024-03-14 01:21:53 +0300

    delete router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 import './styles/header.css';
 import './styles/categories.css';
